9382f368c0bd90f3e09ae54da9775de9359fe262
m0ksemm <md.cheban@gmail.com>
2023-10-31 09:52:08 +0200

    Task 4

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -11,10 +11,32 @@ app.Run(async (HttpContext context) =>
     Dictionary<string, StringValues> queryDict =
         Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
 
-    if (queryDict.ContainsKey("firstName"))
+    if (queryDict.ContainsKey("firstNumber"))
     {
-        string firstName = queryDict["firstName"][0];
-        await context.Response.WriteAsync(firstName);
+        string firstNumber = queryDict["firstNumber"][0];
+        string secondNumber = queryDict["secondNumber"][0];
+        string operation = queryDict["operation"][0];
+
+        int number1 = Int32.Parse(firstNumber);
+        int number2 = Int32.Parse(secondNumber);
+        switch (operation)
+        {
+            case "add":
+                await context.Response.WriteAsync($"<p>{number1 + number2}</p>");
+                break;
+            case "sub":
+                await context.Response.WriteAsync($"<p>{number1 - number2}</p>");
+                break;
+            case "mult":
+                await context.Response.WriteAsync($"<p>{number1 * number2}</p>");
+                break;
+            case "div":
+                await context.Response.WriteAsync($"<p>{number1 / number2}</p>");
+                break;
+            case "rem":
+                await context.Response.WriteAsync($"<p>{number1 % number2}</p>");
+                break;
+        }
     }
 });
 
