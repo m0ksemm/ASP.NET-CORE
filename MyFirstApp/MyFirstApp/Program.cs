666c7de1e3560fe41365d80341d9371fca56e9e8
m0ksemm <md.cheban@gmail.com>
2023-10-31 11:14:32 +0200

    Task4. Final

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -11,67 +11,63 @@ app.Run(async (HttpContext context) =>
     if (body.Length > 2) 
     {
         body = body.Substring(1);
-    }
-        
-
-    //await context.Response.WriteAsync($"<p>{body}</p>");
-
-
-    Dictionary<string, StringValues> queryDict =
+        Dictionary<string, StringValues> queryDict =
         Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
-    if (!queryDict.ContainsKey("firstNumber"))
-    {
-        context.Response.StatusCode = 400;
-        await context.Response.WriteAsync($"<p>Invalid input for 'firstNumber'</p>");
-    }
-    else if (!queryDict.ContainsKey("operation") && !queryDict.ContainsKey("secondNumber"))
-    {
-        context.Response.StatusCode = 400;
-        await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
-        await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
-    }
-    else if (!queryDict.ContainsKey("secondNumber"))
-    {
-        context.Response.StatusCode = 400;
-        await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
-    }
-    else if (!queryDict.ContainsKey("operation"))
-    {
-        context.Response.StatusCode = 400;
-        await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
-    }
-    else 
-    {
-        string firstNumber = queryDict["firstNumber"][0];
-        string secondNumber = queryDict["secondNumber"][0];
-        string operation = queryDict["operation"][0];
-
-        int number1 = Int32.Parse(firstNumber);
-        int number2 = Int32.Parse(secondNumber);
-        switch (operation)
+        if (!queryDict.ContainsKey("firstNumber"))
         {
-            case "add":
-                await context.Response.WriteAsync($"<p>{number1 + number2}</p>");
-                break;
-            case "sub":
-                await context.Response.WriteAsync($"<p>{number1 - number2}</p>");
-                break;
-            case "mult":
-                await context.Response.WriteAsync($"<p>{number1 * number2}</p>");
-                break;
-            case "div":
-                await context.Response.WriteAsync($"<p>{number1 / number2}</p>");
-                break;
-            case "rem":
-                await context.Response.WriteAsync($"<p>{number1 % number2}</p>");
-                break;
-            default:
-                context.Response.StatusCode = 400;
-                await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
-                break;
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"<p>Invalid input for 'firstNumber'</p>");
         }
-    }
+        else if (!queryDict.ContainsKey("operation") && !queryDict.ContainsKey("secondNumber"))
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+            await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
+        }
+        else if (!queryDict.ContainsKey("secondNumber"))
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
+        }
+        else if (!queryDict.ContainsKey("operation"))
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+        }
+        else
+        {
+            string firstNumber = queryDict["firstNumber"][0];
+            string secondNumber = queryDict["secondNumber"][0];
+            string operation = queryDict["operation"][0];
 
+            int number1 = Int32.Parse(firstNumber);
+            int number2 = Int32.Parse(secondNumber);
+            switch (operation)
+            {
+                case "add":
+                    await context.Response.WriteAsync($"<p>{number1 + number2}</p>");
+                    break;
+                case "sub":
+                    await context.Response.WriteAsync($"<p>{number1 - number2}</p>");
+                    break;
+                case "mult":
+                    await context.Response.WriteAsync($"<p>{number1 * number2}</p>");
+                    break;
+                case "div":
+                    await context.Response.WriteAsync($"<p>{number1 / number2}</p>");
+                    break;
+                case "rem":
+                    await context.Response.WriteAsync($"<p>{number1 % number2}</p>");
+                    break;
+                default:
+                    context.Response.StatusCode = 400;
+                    await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+                    break;
+            }
+        }
+    } 
+    
+    
 });
 
 app.Run();
