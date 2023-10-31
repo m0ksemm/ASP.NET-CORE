5e5b5a6dca3f17c1806cc7004ac46d26c0f36206
m0ksemm <md.cheban@gmail.com>
2023-10-31 10:51:37 +0200

    Task 4.

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,13 +5,42 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) => 
 {
-    System.IO.StreamReader reader = new System.IO.StreamReader(context.Request.Body);
-    string body = await reader.ReadToEndAsync();
+    var qs = context.Request.QueryString;
+    string body = qs.ToString();
+
+    if (body.Length > 2) 
+    {
+        body = body.Substring(1);
+    }
+        
+
+    //await context.Response.WriteAsync($"<p>{body}</p>");
+
 
     Dictionary<string, StringValues> queryDict =
         Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
-
-    if (queryDict.ContainsKey("firstNumber"))
+    if (!queryDict.ContainsKey("firstNumber"))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"<p>Invalid input for 'firstNumber'</p>");
+    }
+    else if (!queryDict.ContainsKey("operation") && !queryDict.ContainsKey("secondNumber"))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+        await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
+    }
+    else if (!queryDict.ContainsKey("secondNumber"))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"<p>Invalid input for 'secondNumber'</p>");
+    }
+    else if (!queryDict.ContainsKey("operation"))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+    }
+    else 
     {
         string firstNumber = queryDict["firstNumber"][0];
         string secondNumber = queryDict["secondNumber"][0];
@@ -36,8 +65,13 @@ app.Run(async (HttpContext context) =>
             case "rem":
                 await context.Response.WriteAsync($"<p>{number1 % number2}</p>");
                 break;
+            default:
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync($"<p>Invalid input for 'operation'</p>");
+                break;
         }
     }
+
 });
 
 app.Run();
