9ca2e000979114bd979b3f94b74443a970244d42
m0ksemm <md.cheban@gmail.com>
2023-11-03 13:46:07 +0200

    Constraint example. Continue

--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -20,11 +20,43 @@ app.UseEndpoints(endpoints =>
         string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
         await context.Response.WriteAsync($"In Employee profile - {employeeName}");
     });
+
+    endpoints.Map("products/details/{id:int?}", async context =>
+    {
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            await context.Response.WriteAsync($"Products details - {id}");
+        }
+        else
+        {
+            await context.Response.WriteAsync($"Products details - id is not supplied");
+        }
+    });
+
+    endpoints.Map("daily-digest-report/{reportdate:datetime}", async context => {
+        DateTime reportDate = Convert.ToDateTime(context.Request.RouteValues["reportdate"]);
+        await context.Response.WriteAsync($"In daily-digest-report - {reportDate.ToShortDateString()}");
+    });
+
+    endpoints.Map("cities/{cityid:guid}", async context =>
+    {
+        Guid cityId = Guid.Parse(Convert.ToString(context.Request.RouteValues["cityid"])!);
+        await context.Response.WriteAsync($"City information - {cityId}");
+    });
+
+
+    endpoints.Map("sales-report/{year:int:min(1900)}/{month:regex(^(apr|jul|oct|jan)$)}", async context =>
+    {
+        int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+        string? month = Convert.ToString(context.Request.RouteValues["month"]);
+        await context.Response.WriteAsync($"sales report - {year} - {month}");
+    });
 });
 
 app.Run(async context =>
 {
-    await context.Response.WriteAsync($"Request recieved at {context.Request.Path}\n");
+    await context.Response.WriteAsync($"No route matched at {context.Request.Path}\n");
 });
 
 app.Run();
