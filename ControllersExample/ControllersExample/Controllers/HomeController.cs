a82cbd969685d132f3c3eff0f23709467ad3a19d
m0ksemm <md.cheban@gmail.com>
2023-11-06 15:51:28 +0200

    Controllers example

---
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using ControllersExample.Models;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public ContentResult Index()
+        {
+            //return Content() new ContentResult() { Content = 
+            //"Hello from Index", ContentType = "text/plain"
+            //};
+            //return Content("Hello from Index", "text/plain");
+
+            return Content("<h1>Welcome</h1> <h2>Hello from Index<h2>", "text/html");
+        }
+
+        [Route("person")]
+        public JsonResult Person()
+        {
+            Person person = new Person() {Id =  
+                Guid.NewGuid(), FirstName = "James", LastName = "Smith", Age = 25};
+            //return new JsonResult(person);
+            return Json(person);
+            //return "{ \"key\": \"value\" }";
+        }
+
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+
+        [Route("file-download")]
+        public VirtualFileResult FileFownload()
+        {
+            //return new VirtualFileResult("/sample.pdf", "application/pdf");
+            return File("/sample.pdf", "application/pdf");
+        }
+
+        [Route("file-download2")]
+        public PhysicalFileResult FileFownload2()
+        {
+            //return new PhysicalFileResult(@"C:\Users\User\Desktop\sample.pdf", "application/pdf");
+            return PhysicalFile(@"C:\Users\User\Desktop\sample.pdf", "application/pdf");
+        }
+
+        [Route("file-download3")]
+        public FileContentResult FileFownload3()
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(@"C:\Users\User\Desktop\sample.pdf");
+            //return new FileContentResult(bytes, "application/pdf");
+            return File(bytes, "application/pdf");
+        }
+    }
+}
---
+++ ControllersExample/ControllersExample/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ControllersExample.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
