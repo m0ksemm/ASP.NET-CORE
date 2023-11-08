4d0d4827ac4b93c1bb63cddc1f7c2c162529d167
m0ksemm <md.cheban@gmail.com>
2023-11-08 17:35:23 +0200

    IActionResult example

--- IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace IActionResultExample.Controllers
 {
@@ -17,32 +18,31 @@ namespace IActionResultExample.Controllers
             //Book id can't be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"]))) 
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be null or empty");
+                return BadRequest("Book id can't be null or empty");
             }
 
             //Book id should be between 1 to 1000
             int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
             if (bookId <= 0)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be less than or equal zero");
+                return BadRequest("Book id can't be less than or equal zero");
             }
             if (bookId > 1000) 
             {
-                Response.StatusCode = 400;
-                return Content("Book id can't be greater than 1000");
+                return NotFound("Book id can't be greater than 1000");
             }
 
             //isloggedin should be true
             if (Convert.ToBoolean(Request.Query["isloggedin"]) == false) 
             {
-                Response.StatusCode = 401;
-                return Content("User must be authenticated");
+                //return Unauthorized("User must be authenticated");
+                return StatusCode(401);
             }
 
-            return File("/sample.pdf", "application/pdf");
+            //return File("/sample.pdf", "application/pdf");
+            return new RedirectToActionResult("Books", "Store", new { }, true);
 
         }
     }
 }
+--
