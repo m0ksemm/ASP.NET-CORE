7378a64bd1d802d4446f9eb83e9bb99b71828e29
m0ksemm <md.cheban@gmail.com>
2023-11-08 17:35:57 +0200

    Bank Application

---
+++ BankApplication/BankApplication/Controllers/MainController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using BankApplication.Models;
+
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+
+namespace BankApplication.Controllers
+{
+    public class MainController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return Content("Wellcome to the Best Bank!");
+        }
+
+        [Route("account-details")]      
+        public JsonResult Details()
+        {
+            Details first = new Details() { accountNumber = 1001, accountHolderName = "Example Name\n", currentBalance = 5000 };
+            return Json(first);
+        }
+        [Route("account-statement")]
+        public VirtualFileResult Statement()
+        {
+            return File("/Sample.pdf", "application/pdf");
+        }
+
+        [Route("get-current-balance/{accountNumber}")]
+        public IActionResult currentBalance()
+        {
+            int numb = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+            if (numb != 1001)
+            {
+                return BadRequest($"{numb}:Account Number should be 1001");
+            }
+            else
+            {
+                return Content("5000", "text/plain");
+            }
+        }
+
+        [Route("get-current-balance")]
+        public IActionResult error()
+        {
+            return NotFound("Account Number should be supplied");
+        }
+
+    }
+
+}
---
+++ BankApplication/BankApplication/Models/Details.cs
@@ -0,0 +1,9 @@
+﻿namespace BankApplication.Models
+{
+    public class Details
+    {
+        public int accountNumber { get; set; }
+        public string accountHolderName { get; set; }
+        public int currentBalance { get; set; }
+    }
+}
