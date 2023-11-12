6ce9921d5ae9954d1b6481a82bd7fede9b4d0138
m0ksemm <md.cheban@gmail.com>
2023-11-12 15:34:53 +0200

    Model validation example

---
+++ ModelValidationsExample/ModelValidationsExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationsExample.Models;
+
+namespace ModelValidationsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Password), nameof(Person.ConfirmPassword))]
+        [Route("register")]
+        public IActionResult Index(Person person)
+        {
+            if (!ModelState.IsValid) 
+            {
+                string errors = string.Join("\n", 
+                ModelState.Values.SelectMany(value =>
+                value.Errors).Select(err => err.ErrorMessage));
+                
+                return BadRequest(errors);
+            }
+
+            return Content($"{person}");
+        }
+    }
+}
