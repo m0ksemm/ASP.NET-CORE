00ca87923149cba465895174b743cc4102886ab7
m0ksemm <md.cheban@gmail.com>
2023-11-15 01:19:45 +0200

    view example

---
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(); //Index.cshtml
+            //return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}
