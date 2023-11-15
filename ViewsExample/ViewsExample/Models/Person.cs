935e23d9b63a47bade86becff5c96eac00a47044
m0ksemm <md.cheban@gmail.com>
2023-11-15 14:09:27 +0200

    View result example, if+switch

--- ViewsExample/ViewsExample/Models/Person.cs
+++ ViewsExample/ViewsExample/Models/Person.cs
@@ -3,6 +3,12 @@
     public class Person
     {
         public string? Name { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Gender PersonGender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male, Female, Other
     }
 }
