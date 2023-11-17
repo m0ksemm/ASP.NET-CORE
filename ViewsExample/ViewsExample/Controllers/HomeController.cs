794d605dede9002129782bf4dc1e8574e9554ed3
m0ksemm <md.cheban@gmail.com>
2023-11-17 07:12:53 +0200

    View examples. + ViewBag/ViewData

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -8,8 +9,55 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            return View(); //Index.cshtml
+            ViewData["appTitle"] = "Asp.Net Core App";
+            List<Person> people = new List<Person>()
+            {
+                new Person() {Name = "John", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male},
+                new Person() {Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female},
+                new Person() {Name = "Susan", DateOfBirth = DateTime.Parse("2008-07-12"), PersonGender = Gender.Other}
+            };
+            //ViewData["people"] = people;
+            //ViewBag.people = people;
+
+            return View("Index", people); //Index.cshtml
             //return new ViewResult() { ViewName = "abc" };
+        }
+
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if (name == null)
+                return Content("Person name can't be null");
+
+            List<Person> people = new List<Person>()
+            {
+                new Person() {Name = "John", DateOfBirth = DateTime.Parse("2000-05-06"), PersonGender = Gender.Male},
+                new Person() {Name = "Linda", DateOfBirth = DateTime.Parse("2005-01-09"), PersonGender = Gender.Female},
+                new Person() {Name = "Susan", DateOfBirth = DateTime.Parse("2008-07-12"), PersonGender = Gender.Other}
+            };
+            Person? matchingPerson = people.Where(temp => temp.Name == name).FirstOrDefault();
+            return View(matchingPerson);  //Views/Home/Details.cshtml
+        }
+
+        [Route("person-with-product")]
+        public IActionResult PersonWithProduct()
+        {
+            Person person = new Person() { Name = "Sara", 
+                PersonGender = Gender.Female, DateOfBirth = Convert.ToDateTime("2004-07-01") };
+            Product product = new Product() { ProductId = 1, ProductName = "Air Conditioner" };
+            
+            PersonAndProductWrapperModel personAndProductWrapperModel 
+                = new PersonAndProductWrapperModel() { PersonData = person, ProductData = product};
+            return View(personAndProductWrapperModel);
+        }
+
+
+        [Route("home/all-products")]
+        public IActionResult All()
+        {
+            return View();
+            //Views/Home/All.cshtml
+            //Views/Shared/All.cshtml
         }
     }
 }
