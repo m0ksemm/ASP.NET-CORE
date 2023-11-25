82bb39fc9a371b4a0ef0905323455b15f2b63ded
m0ksemm <md.cheban@gmail.com>
2023-11-26 00:37:37 +0200

    Layout views

---
+++ LayoutViews/LayoutViews/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Url: /search-products/1
+        [Route("search-products/{ProductId?}")] 
+        public IActionResult Search(int? ProductId)
+        {
+            ViewBag.ProductID = ProductId;
+            return View();
+        }
+
+        [Route("order-product")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
