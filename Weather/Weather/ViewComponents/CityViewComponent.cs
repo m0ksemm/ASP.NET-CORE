2dd772b1a13e69e14024b0cde0d2c3d40747eae9
m0ksemm <md.cheban@gmail.com>
2023-11-29 13:45:38 +0200

    Task 20. ViewComponents, Weather app

---
+++ Weather/Weather/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Weather.Models;
+
+namespace Weather.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(WeatherInfo weatherinfo)
+        {
+
+            return View("Sample", weatherinfo); //invoked a partial view Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}
