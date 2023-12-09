41817187804f4741a595aee74012359b81324251
m0ksemm <md.cheban@gmail.com>
2023-12-09 19:04:19 +0200

    DiE

---
+++ DIExample/DIExample/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using ServiceContracts;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DIExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICitiesService _citiesService1;
+        private readonly ICitiesService _citiesService2;
+        private readonly ICitiesService _citiesService3;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        //constructor
+        public HomeController(ICitiesService citiesService1, ICitiesService citiesService2, ICitiesService citiesService3, IServiceScopeFactory serviceScopeFactory)
+        {
+            _citiesService1 = citiesService1;
+            _citiesService2 = citiesService2;
+            _citiesService3 = citiesService3;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+        
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<string> cities = _citiesService1.GetCities();
+            ViewBag.InstanceId_CitiesService_1 = _citiesService1.ServiceInstanceId;
+            ViewBag.InstanceId_CitiesService_2 = _citiesService2.ServiceInstanceId;
+            ViewBag.InstanceId_CitiesService_3 = _citiesService3.ServiceInstanceId;
+
+            using (IServiceScope scope = _serviceScopeFactory.CreateScope()) 
+            {
+                //Inject CitieService
+                ICitiesService citiesService = scope.ServiceProvider.GetRequiredService<ICitiesService>();
+                //DB work
+
+                ViewBag.InstanceId_CitiesService_InScope = citiesService.ServiceInstanceId;
+            } //end of scope; it calls CitiesServiceDispose()
+
+                return View(cities);
+        }
+    }
+}
---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,38 @@
+﻿using ServiceContracts;
+namespace Services
+{
+    public class CitiesService : ICitiesService, IDisposable 
+    {
+        private List<string> _cities;
+        private Guid _serviceInstanceId;
+        public Guid ServiceInstanceId { 
+            get 
+            {
+                return _serviceInstanceId;
+            } 
+        }
+        public CitiesService() 
+        {
+            _serviceInstanceId = Guid.NewGuid();
+            _cities = new List<string>()
+            {
+                "London",
+                "Paris",
+                "New York",
+                "Tokyo",
+                "Rome"
+            };
+            //TO DO: Add logic open db connection
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+        public void Dispose()
+        {
+            //TO DO: add logic to close connection
+        }
+    }
+}
