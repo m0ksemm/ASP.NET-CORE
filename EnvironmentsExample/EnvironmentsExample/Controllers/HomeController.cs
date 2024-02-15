2a99fc1f804222607585bf810eb37bd324eecc43
m0ksemm <md.cheban@gmail.com>
2024-02-15 11:22:36 +0200

    EnvironmetsExample

---
+++ EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("/")]
+        [Route("some-route")]
+        public IActionResult Index()
+        {
+            ViewBag.CurrentEnvironment = _webHostEnvironment.EnvironmentName;
+            return View();
+        }
+    }
+}
