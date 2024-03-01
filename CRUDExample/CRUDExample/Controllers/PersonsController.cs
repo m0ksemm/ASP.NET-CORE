3314c81ca78298fe17081f82280a013572af3fcc
m0ksemm <md.cheban@gmail.com>
2024-03-01 06:30:27 +0200

    Added Search and Sort functionality

--- CRUDExample/CRUDExample/Controllers/PersonsController.cs
+++ CRUDExample/CRUDExample/Controllers/PersonsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 using System.Net;
 
 namespace CRUDExample.Controllers
@@ -18,11 +19,27 @@ namespace CRUDExample.Controllers
 
         [Route("persons/index")]
         [Route("/")]
-        public IActionResult Index()
+        public IActionResult Index(string searchBy, string? searchString, string sortBy = nameof(PersonResponse.PersonName), SortOrderOptions sortOrder = SortOrderOptions.ASC)
         {
-            List<PersonResponse> persons = _personsService.GetAllPersons();
+            //Search
+            ViewBag.SearchFields = new Dictionary<string, string>()
+            {
+                { nameof(PersonResponse.PersonName), "Person Name" },
+                { nameof(PersonResponse.Email), "Email" },
+                { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+                { nameof(PersonResponse.Gender), "Gender" },
+                { nameof(PersonResponse.CountryID), "Country" },
+                { nameof(PersonResponse.Address), "Address" },
+            };
+            List<PersonResponse> persons = _personsService.GetFilteredPerson(searchBy, searchString);
+            ViewBag.CurrentSearchBy = searchBy;
+            ViewBag.CurrentSearchString = searchString;
 
-            return View(persons); //Views/Persons/Index.cshtml
+            //Sort
+            List<PersonResponse> sortedPersons = _personsService.GetSortedPersons(persons, sortBy, sortOrder);
+            ViewBag.CurrentSortBy = sortBy;
+            ViewBag.CurrentSortOrder = sortOrder.ToString();
+            return View(sortedPersons); //Views/Persons/Index.cshtml
         }
     }
 }
--- CRUDExample/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
+++ CRUDExample/CRUDExample/obj/Debug/net8.0/CRUDExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b6ff80d793874d94a879b0689b09d34eb6b435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9706d8dcd393066825ed040d9ffe6a96cc82e35e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
