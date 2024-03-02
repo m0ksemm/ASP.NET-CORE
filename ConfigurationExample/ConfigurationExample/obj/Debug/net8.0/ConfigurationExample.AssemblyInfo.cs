3170662b8335b0297c2e8d7275940d3116607d52
m0ksemm <md.cheban@gmail.com>
2024-03-02 11:34:48 +0200

    Configuretion example

--- ConfigurationExample/ConfigurationExample/obj/Debug/net8.0/ConfigurationExample.AssemblyInfo.cs
+++ ConfigurationExample/ConfigurationExample/obj/Debug/net8.0/ConfigurationExample.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83046889109f23d04b1324665e803f1f4710790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigurationExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
