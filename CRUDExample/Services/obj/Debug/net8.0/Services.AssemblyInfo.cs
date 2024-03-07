c11b34ea947b5899e283f438aaabdb8c2ec3fad2
m0ksemm <md.cheban@gmail.com>
2024-03-07 16:50:48 +0200

    Added data validation through jQuery, added tag-helpers for it.

--- CRUDExample/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ CRUDExample/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e971a19b43299f59e8575bd9d824f42ba00bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca2953392b208e00938aea26a58386c8b8973ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
